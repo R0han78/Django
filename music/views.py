ee24ab9c73fb2de8188be85fbe19e9fcb2664f99
R0han78 <sambuddha.nath@gmail.com>
2017-05-13 12:50:53 +0530

    First Commit with basic layout of music file sharing system

---
+++ music/views.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse,Http404
+from .models import Album
+
+# Create your views here.
+
+#type 1
+#def index(request):
+#    all_albums = Album.objects.all()
+#    html=''
+#    for album in all_albums:
+#        url = '/music/'+str(album.id)+'/'
+#        html+='<a href="'+url+'">'+album.album_title+'</a><br>'
+#    return HttpResponse(html)
+
+#type 2
+#from django.template import loader
+#def index(request):
+#    all_albums = Album.objects.all()
+#    template = loader.get_template('music/index.html')
+#    context = {
+#        'all_albums' : all_albums,
+#    }
+#    return HttpResponse(template.render(context,request))
+
+#type3
+from django.shortcuts import render
+def index(request):
+    all_albums = Album.objects.all()
+    context = {
+        'all_albums' : all_albums,
+    }
+    return render(request,'music/index.html',context)
+
+
+
+def detail(request,album_id):
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album Not Present")
+    context = {
+        'album' : album,
+    }
+    return render(request,'music/details.html',context)
+
+
+
+
+
